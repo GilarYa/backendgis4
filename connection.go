cb18c9fe69881f0025d238b26dbc0d088cbf23c8
gilarwahibul <gilarwahibul@gmail.com>
2023-11-17 17:38:15 +0700

    push

--- connection.go
+++ connection.go
@@ -2,10 +2,11 @@ package backendgis4
 
 import (
 	"context"
+	"os"
+
 	"github.com/aiteung/atdb"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"os"
 )
 
 func GetConnectionMongo(MongoString, dbname string) *mongo.Database {
@@ -22,7 +23,7 @@ func GetAllData(MongoConnect *mongo.Database, colname string) []GeoJson {
 	return data
 }
 
-func InsertDatajson(MongoConn *mongo.Database, colname string, coordinate []float64, name, volume, tipe string) (InsertedID interface{}) {
+func InsertDatajson(MongoConn *mongo.Database, colname string, coordinate [][]float64, name, volume, tipe string) (InsertedID interface{}) {
 	req := new(LonLatProperties)
 	req.Type = tipe
 	req.Coordinates = coordinate
@@ -34,29 +35,28 @@ func InsertDatajson(MongoConn *mongo.Database, colname string, coordinate []floa
 }
 
 func UpdateDatajson(MongoConn *mongo.Database, colname, name, newVolume, newTipe string) error {
-    filter := bson.M{"name": name}
+	filter := bson.M{"name": name}
 
+	update := bson.M{
+		"$set": bson.M{
+			"volume": newVolume,
+			"tipe":   newTipe,
+		},
+	}
 
-    update := bson.M{
-        "$set": bson.M{
-            "volume": newVolume,
-            "tipe":   newTipe,
-        },
-    }
+	_, err := MongoConn.Collection(colname).UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
 
-    _, err := MongoConn.Collection(colname).UpdateOne(context.TODO(), filter, update)
-    if err != nil {
-        return err
-    }
-
-    return nil
+	return nil
 }
 
 func DeleteDatajson(MongoConn *mongo.Database, colname string, name string) (*mongo.DeleteResult, error) {
-    filter := bson.M{"name": name}
-    del, err := MongoConn.Collection(colname).DeleteOne(context.TODO(), filter)
-    if err != nil {
-        return nil, err
-    }
-    return del, nil
+	filter := bson.M{"name": name}
+	del, err := MongoConn.Collection(colname).DeleteOne(context.TODO(), filter)
+	if err != nil {
+		return nil, err
+	}
+	return del, nil
 }
